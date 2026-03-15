757f9195c8523be454075acf48d18aec1403f3b6
Pixel-Pages <268321651+Pixel-Pages@users.noreply.github.com>
2026-03-15 18:16:20 +0100

    configure vite for pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/Camp-Layout-Tool/',
   plugins: [react()],
   test: {
     environment: 'jsdom',
